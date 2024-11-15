4624f9baa5794333f5920771eacfbe9415704bd6
anudeepflyflair <anudeep.yesupogu@flyflair.com>
2024-11-15 00:02:50 -0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "full_api_url" {
   value       = "https://${aws_api_gateway_domain_name.custom_domain.domain_name}/${aws_api_gateway_stage.api_stage.stage_name}/payment-gateway/process-payment"
   description = "The full URL for the /payment-gateway/process-payment endpoint with custom domain and stage"
 }
----------------------------------
+
 import hmac
 import hashlib
 import base64
